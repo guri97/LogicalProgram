39432a56e31bc02513231139b55b2564a8d3639b
guri97 <guri84096@gmail.com>
2022-06-10 11:36:43 +0530

    [gurpreet] UC1_Stopwatch

---
+++ LogicalProgram/LogicalProgram/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace LogicalProgram
+{
+    class logicalProgram
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Enter a Number to Choose Respective Problem, \n");
+            Console.WriteLine("Enter 1 for Stopwatch");
+
+            int Option = Convert.ToInt32(Console.ReadLine());
+
+            switch (Option)
+            {
+                case 1:
+                    Stopwatch stopwatch = new Stopwatch();
+                    Stopwatch.Stopwatchprg();
+                    break;
+
+            }
+        }
+    }
+}
+
---
+++ LogicalProgram/LogicalProgram/Stopwatch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProgram
+{
+    internal class Stopwatch
+    {
+        public static void Stopwatchprg()
+        {
+            Console.WriteLine("Press Enter to Start.");
+            Console.WriteLine();
+            DateTime Start = DateTime.Now;
+
+            Console.WriteLine("Press Enter to Stop.");
+            Console.ReadLine();
+            DateTime Stop = DateTime.Now;
+
+            Console.Write("Elapsed Time = " + (Stop - Start));
+        }
+    }
+}
